b7eab385fabd64d3554672c6a20e9d76fef038d0
Joe <a0980162600@gmail.com>
2026-06-08 20:53:15 -0700

    24+24

--- lib/main.dart
+++ lib/main.dart
@@ -37887,8 +37887,25 @@ class CashSessionHandHistoryPage extends StatelessWidget {
             .orderBy('createdAt', descending: true)
             .snapshots(),
         builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error: ${snapshot.error}',
+              ),
+            );
+          }
+
+          if (snapshot.connectionState ==
+              ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
           if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
+            return const Center(
+              child: Text('No data'),
+            );
           }
 
           final docs = snapshot.data!.docs;
@@ -38107,8 +38124,25 @@ class TournamentHandHistoryPage extends StatelessWidget {
             .orderBy('createdAt', descending: true)
             .snapshots(),
         builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Error: ${snapshot.error}',
+              ),
+            );
+          }
+
+          if (snapshot.connectionState ==
+              ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
           if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
+            return const Center(
+              child: Text('No data'),
+            );
           }
 
           final docs = snapshot.data!.docs;
